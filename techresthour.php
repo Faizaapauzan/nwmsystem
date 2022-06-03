1ec5263c8c5fdacebfafaa60a95ef80784f67b42
Faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2022-06-03 11:59:28 +0800

    faiza 3/6/2022

--- techresthour.php
+++ techresthour.php
@@ -207,7 +207,7 @@
           $technician = $_GET['technician'];
           $assistant = $_GET['assistant'];
 
-          $query = "SELECT * FROM technician_resthour WHERE technician='$technician' AND assistant='$assistant' ORDER BY resthour_id DESC";
+          $query = "SELECT * FROM technician_resthour WHERE technician='$technician' AND assistant='$assistant' ORDER BY resthour_id DESC LIMIT 1";
           $query_run = mysqli_query($con, $query);
 
           if(mysqli_num_rows($query_run) > 0){
