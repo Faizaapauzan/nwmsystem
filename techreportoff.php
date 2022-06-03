579702e59e41ae27da5fb8393606f00a5e77f3cf
Imranmuazam <ithinkimran.mfgsoln@gmail.com>
2022-06-03 12:05:40 +0800

    latest

--- techreportoff.php
+++ techreportoff.php
@@ -3,6 +3,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel = "icon" href = "https://i.ibb.co/ngKJ7c4/android-chrome-512x512.png" type = "image/x-icon">
 	<title>Technician Report Off</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
