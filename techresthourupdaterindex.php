6390b44e5c27cc1d11c6f0480e6918f942efd195
Faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2022-06-15 17:28:34 +0800

    faiza 15/6/2022

--- techresthourupdaterindex.php
+++ techresthourupdaterindex.php
@@ -8,9 +8,21 @@ if(isset($_POST['tech_out']) && $_POST['tech_out']!='' || $_POST['tech_out']==''
     &&
    isset($_POST['tech_in']) && $_POST['tech_in']!='' || $_POST['tech_in']==''
     &&
-   isset($_POST['ass_out']) && $_POST['ass_out']!='' || $_POST['ass_out']==''
+   isset($_POST['Ass1_out']) && $_POST['Ass1_out']!='' || $_POST['Ass1_out']==''
     &&
-   isset($_POST['ass_in']) && $_POST['ass_in']!='' || $_POST['ass_in']=='' 
+   isset($_POST['Ass1_in']) && $_POST['Ass1_in']!='' || $_POST['Ass1_in']=='' 
+    &&
+    isset($_POST['Ass2_out']) && $_POST['Ass2_out']!='' || $_POST['Ass2_out']==''
+    &&
+   isset($_POST['Ass2_in']) && $_POST['Ass2_in']!='' || $_POST['Ass2_in']=='' 
+    &&
+    isset($_POST['Ass3_out']) && $_POST['Ass3_out']!='' || $_POST['Ass3_out']==''
+    &&
+   isset($_POST['Ass3_in']) && $_POST['Ass3_in']!='' || $_POST['Ass3_in']=='' 
+    &&
+    isset($_POST['Ass4_out']) && $_POST['Ass4_out']!='' || $_POST['Ass4_out']==''
+    &&
+   isset($_POST['Ass4_in']) && $_POST['Ass4_in']!='' || $_POST['Ass4_in']=='' 
     &&
    isset($_POST['resthour_id']) && $_POST['resthour_id']!='' || $_POST['resthour_id']=='')
 
@@ -19,8 +31,14 @@ if(isset($_POST['tech_out']) && $_POST['tech_out']!='' || $_POST['tech_out']==''
         $sql = "UPDATE technician_resthour SET
                        tech_out ='".addslashes($_POST['tech_out'])."',
                        tech_in ='".addslashes($_POST['tech_in'])."',
-                       ass_out ='".addslashes($_POST['ass_out'])."',
-                       ass_in ='".addslashes($_POST['ass_in'])."'
+                       Ass1_out ='".addslashes($_POST['Ass1_out'])."',
+                       Ass1_in ='".addslashes($_POST['Ass1_in'])."',
+                       Ass2_out ='".addslashes($_POST['Ass2_out'])."',
+                       Ass2_in ='".addslashes($_POST['Ass2_in'])."',
+                       Ass3_out ='".addslashes($_POST['Ass3_out'])."',
+                       Ass3_in ='".addslashes($_POST['Ass3_in'])."',
+                       Ass4_out ='".addslashes($_POST['Ass4_out'])."',
+                       Ass4_in ='".addslashes($_POST['Ass4_in'])."'
                 WHERE  resthour_id ='".addslashes($_POST['resthour_id'])."' ";
         
         if($conn->query($sql))
