ae88be751edfa9a7929d2c098ecd30555683adee
iThinkFarisha <ithinkfarisha.mfgsoln@gmail.com>
2022-07-26 16:45:02 +0800

    farisha 260722

--- accessories.php
+++ accessories.php
@@ -46,7 +46,7 @@ $query = $conn->query("SELECT * FROM accessories_list ORDER BY accessories_id AS
    
    <!-- Script -->
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src='bootstrap/js/bootstrap.bundle.min.js' type='text/javascript'></script> 
+    <!-- <script src='bootstrap/js/bootstrap.bundle.min.js' type='text/javascript'></script>  -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
