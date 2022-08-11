7ca932213f55e989bdc96388aa203af1093984b9
Faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2022-08-11 15:26:47 +0800

    faiza 11/8/2022

--- dblogin.php
+++ dblogin.php
@@ -1,79 +1,75 @@
-<?php
-
-session_start();
-
-
-
+<?php session_start();
 
 include 'dbconnect.php';
-
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-
-$sql = mysqli_query($conn,"SELECT * FROM staff_register WHERE username='$username' and password='$password'");
+$sql = mysqli_query($conn,"SELECT * FROM staff_register WHERE username='$username'");
 
 $cek = mysqli_num_rows($sql);
 
-
-
-
-if($cek > 0){
+if($cek > 0) {
 
  $data = mysqli_fetch_assoc($sql);
 
-
- if($data['staff_position']=="Admin"){
+ if($data['staff_position']=="Admin" AND password_verify($password, $data['password'])) {
 
   $_SESSION['username'] = $username;
   $_SESSION['staff_position'] = "Admin";
 
   header("location:Adminhomepage.php");
+  }
 
-  }else if($data['username']=="KEONG"){
+  else if($data['username']=="KEONG" AND password_verify($password, $data['password'])){
 	  
 	  $_SESSION['username'] = $username;
-	  header("location:neosaukeong.php"); 
-	  
-  }else if($data['staff_position']=="Manager"){
+	  header("location:neosaukeong.php");
+
+  }
+  
+  else if($data['staff_position']=="Manager" AND password_verify($password, $data['password'])){
 
   $_SESSION['username'] = $username;
   $_SESSION['staff_position'] = "Manager";
   
   header("location:Adminhomepage.php");
-
-
- }else if($data['staff_position']=="Technician" AND $data['technician_rank']=="1st Leader"){
+ }
+ 
+ else if($data['staff_position']=="Technician" AND $data['technician_rank']=="1st Leader" AND password_verify($password, $data['password'])){
 
   $_SESSION['username'] = $username;
   $_SESSION['staff_position'] = "Technician";
   $_SESSION['technician_rank'] = "1st Leader"; 
   
   header("location:technician.php");
-
- }else if($data['staff_position']=="Technician" AND $data['technician_rank']=="2nd Leader"){
+ }
+ 
+ else if($data['staff_position']=="Technician" AND $data['technician_rank']=="2nd Leader" AND password_verify($password, $data['password'])){
 	
   $_SESSION['username'] = $username;
   $_SESSION['staff_position'] = "Technician";
   $_SESSION['technician_rank'] = "2nd Leader"; 
   
   header("location:technician.php");	
-	 
-	 
- }else if($data['staff_position']=="Storekeeper"){
+ }
+ 
+ else if($data['staff_position']=="Storekeeper" AND password_verify($password, $data['password'])) {
 	 
 	$_SESSION['username'] = $username;
 	$_SESSION['staff_position'] = "Storekeeper";
 	header("location:store.php");
+}
 
-}else{
-
- 
+else {
   header("location:index.php?error");
- } 
-}else{
+ }
+
+}
+
+else {
  header("location:index.php?error");
 }
+
 ?>
 
