78f54dac6be6ab60427986cef8b49dc46a43e84f
iThinkFarisha <ithinkfarisha.mfgsoln@gmail.com>
2022-08-29 09:40:20 +0800

    Farisha 290822

--- servicereport.php
+++ servicereport.php
@@ -370,7 +370,7 @@ tr td:first-child:before {
             while ($row = mysqli_fetch_array($query_run)) {
     ?>
 
-    <p><label>Service Engineer :</label><span><input type="text" name="job_assign" value="<?php foreach($query_run as $res) :?><?php echo $res['job_assign'] ?> / <?php endforeach;?>" class="input"/></span></p>
+    <p><label style="position:absolute;">Service Engineer :</label><span style="font-size: 13px; width: 207px; height:13px; font-family: Arial; border-width: 0px; resize: none; overflow: hidden; margin-left: 127px;" ><input type="text" name="job_assign" value="<?php foreach($query_run as $res) :?><?php echo $res['job_assign'] ?> / <?php endforeach;?>" class="input"/></span></p>
 
     <?php } } }?>
 
@@ -494,7 +494,7 @@ tr td:first-child:before {
 
     <div class="additionalside">
     <p>Additional Info :-</p>
-    <textarea name="Submitted_Items" class="infoarea" id="autoresizing_item">
+    <textarea name="Submitted_Items" class="infoarea" id="textarea-container">
     
     <?php
 
@@ -571,14 +571,23 @@ tr td:first-child:before {
     
     </textarea>
 
-     <script type="text/javascript">
-        $('#autoresizing_item').on('input', function () {
-            this.style.height = '92px';
-              
-            this.style.height = 
-                    (this.scrollHeight) + 'px';
-        });
-     </script>
+
+<script type="text/javascript">
+var $textArea = $("#textarea-container");
+
+// Re-size to fit initial content.
+resizeTextArea($textArea);
+
+// Remove this binding if you don't want to re-size on typing.
+$textArea.off("keyup.textarea").on("keyup.textarea", function() {
+    resizeTextArea($(this));
+});
+
+function resizeTextArea($element) {
+    $element.height($element[0].scrollHeight);
+}
+  </script>
+     
 
     </div></div>
 
