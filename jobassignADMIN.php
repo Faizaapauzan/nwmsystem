3816a2a2942bdd2eb45dffbdca3e7f0a0deaf9af
iThinkFarisha <ithinkfarisha.mfgsoln@gmail.com>
2022-10-27 10:55:04 +0800

    farisha 271022

--- jobassignADMIN.php
+++ jobassignADMIN.php
@@ -51,9 +51,7 @@
 
     <input type="hidden" id='jobassign' onchange="GetJobAss(this.value)">
     <input type="hidden" name="job_assign" id='username' value="<?php echo $row['job_assign']?>">
-    <input type="hidden" name="technician_rank" id='technician_rank' value="<?php echo $row['technician_rank']?>" readonly>  
-    <input type="hidden" name="staff_position" id='staff_position' value="<?php echo $row['staff_position']?>" readonly>
-  
+
     </select>
   
     <?php
@@ -170,7 +168,6 @@
   <script>
       $("#multipleassist").on('change', function () {
         $(".multiple-assistant").select2({
-          //maximumSelectionLength: 2
         });
       });
   </script>
@@ -259,60 +256,6 @@ $("#jobassignto").on("change",function(){
  });
 });
 </script>
-
-<script>
-		// onkeyup event will occur when the user
-		// release the key and calls the function
-		// assigned to this event
-		function GetJobAss(str) {
-			if (str.length == 0) {
-				document.getElementById("username").value = "";
-                document.getElementById("technician_rank").value = "";
-                 document.getElementById("staff_position").value = "";
-				return;
-			}
-			else {
-
-				// Creates a new XMLHttpRequest object
-				var xmlhttp = new XMLHttpRequest();
-				xmlhttp.onreadystatechange = function () {
-
-					// Defines a function to be called when
-					// the readyState property changes
-					if (this.readyState == 4 &&
-							this.status == 200) {
-						
-						// Typical action to be performed
-						// when the document is ready
-						var myObj = JSON.parse(this.responseText);
-
-						// Returns the response data as a
-						// string and store this array in
-						// a variable assign the value
-						// received to first name input field
-						
-						document.getElementById
-							("username").value = myObj[0];
-						
-						// Assign the value received to
-						// last name input field
-						document.getElementById(
-							"technician_rank").value = myObj[1];
-
-                            document.getElementById(
-							"staff_position").value = myObj[2];
-                            
-					}
-				};
-
-				// xhttp.open("GET", "filename", true);
-				xmlhttp.open("GET", "fetchtechnicianrank.php?staffregister_id=" + str, true);
-				
-				// Sends the request to the server
-				xmlhttp.send();
-			}
-		}
-	</script>
 
 </body>
 </html>
