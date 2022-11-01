c32b6a18ef4cce9c4babcb04a93f1c5d158aa8b6
Faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2022-11-01 09:54:07 +0800

    faiza 1/11/2022

--- Adminhomepage.php
+++ Adminhomepage.php
@@ -315,15 +315,9 @@ if(!isset($_SESSION['username']))
                                 $results = $conn->query("SELECT * FROM job_register WHERE
                                                         (accessories_required IS NULL AND job_status IS NULL AND job_assign IS NULL AND job_cancel IS NULL
                                                             OR
-                                                         accessories_required = 'NO' AND job_status = '' AND job_assign = '' AND job_cancel = ''
-                                                            OR
                                                          accessories_required = 'NO' AND job_status IS NULL AND job_assign IS NULL AND job_cancel IS NULL
                                                             OR
-                                                         staff_position = 'Storekeeper' AND job_status = 'Ready' AND job_cancel = ''
-                                                            OR
                                                          staff_position = 'Storekeeper' AND job_status = 'Ready' AND job_cancel IS NULL
-                                                            OR
-                                                         job_assign = '' AND job_status = 'Ready' AND job_cancel = ''
                                                             OR
                                                          job_assign IS NULL AND job_status = 'Ready' AND job_cancel IS NULL)
                                                         ORDER BY jobregisterlastmodify_at DESC LIMIT 50");
