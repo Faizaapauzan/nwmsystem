eeb55cf31d1221cb59720081c97d55555d510286
iThinkFarisha <ithinkfarisha.mfgsoln@gmail.com>
2022-11-01 16:46:08 +0800

    farisha 011122

--- techresthourindex.php
+++ techresthourindex.php
@@ -4,15 +4,15 @@ include 'dbconnect.php';
 
 $response = array('success' => false);
 
-if(isset($_POST['technician']) && $_POST['technician']!='' || $_POST['technician']==''
+if(isset($_POST['techname']) && $_POST['techname']!='' || $_POST['techname']==''
     &&
    isset($_POST['today_date']) && $_POST['today_date']!='' || $_POST['today_date']=='')
 
     {
         
-        $sql = "INSERT INTO technician_resthour (technician, today_date) 
+        $sql = "INSERT INTO technician_resthour (techname, today_date) 
                        
-                       VALUES ('".addslashes($_POST['technician'])."',
+                       VALUES ('".addslashes($_POST['techname'])."',
                                '".addslashes($_POST['today_date'])."')";
         
         if($conn->query($sql))
