c6f673e31f5a1bf395336462185c5fa6e416056b
Faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2022-11-03 14:06:08 +0800

    faiza 3/11/2022

--- technician.php
+++ technician.php
@@ -152,6 +152,8 @@ session_start();
 						OR
 						job_assign ='{$_SESSION['username']}' AND  job_status IS NULL AND job_cancel IS NULL
 						OR
+						job_assign ='{$_SESSION['username']}' AND  job_status = '' AND job_cancel IS NULL
+						OR
 						job_assign ='{$_SESSION['username']}' AND  job_status = 'Ready' AND job_cancel IS NULL
 						ORDER BY jobregisterlastmodify_at
 						DESC LIMIT 50";
