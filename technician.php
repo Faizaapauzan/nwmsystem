5103f9c1da156698ea41091fa9a8d38da1d56465
Faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2022-11-09 12:43:50 +0800

    faiza 9/11/2022

--- technician.php
+++ technician.php
@@ -37,7 +37,7 @@ session_start();
     <link href="css/technicianmain.css" rel="stylesheet" />
     <!-- Script -->
 	  
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" ></script>
      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js"></script>
