c7e9ceee14f71516798cb375e9ebba857bb53c6a
Faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2022-12-02 12:33:15 +0800

    faiza 2/12/2022

--- NeoSauKeongAttendance.php
+++ NeoSauKeongAttendance.php
@@ -9,7 +9,6 @@
     <link rel = "icon" href = "https://i.ibb.co/ngKJ7c4/android-chrome-512x512.png" type = "image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap4.min.css"/>
-    <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -20,7 +19,6 @@
     <link href="css/technicianmain.css" rel="stylesheet" />
     <link href="css/style.css" rel="stylesheet" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src='bootstrap/js/bootstrap.bundle.min.js' type='text/javascript'></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/cd421cdcf3.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -77,13 +75,13 @@
                 </div>
                 <table class="conversion-rate-table">
                     <thead class="header-section">
-                        <tr class="headers">
-                            <th class="header" scope="col">Leader</th>
-                            <th class="header" scope="col">Assistant</th>
-                            <th class="header" scope="col">Clock In</th>
-                            <th class="header" scope="col">Clock Out</th>
-                            <th class="header" scope="col">Rest Out</th>
-                            <th class="header" scope="col">Rest In</th>
+                        <tr>
+                            <th scope="col">Leader</th>
+                            <th scope="col">Assistant</th>
+                            <th scope="col">Clock In</th>
+                            <th scope="col">Clock Out</th>
+                            <th scope="col">Rest Out</th>
+                            <th scope="col">Rest In</th>
                         </tr>
                     </thead>
                 
@@ -104,12 +102,12 @@
                 
                 <tbody class="data-section">
                     <tr class="ad">
-                        <td class="cell"><?php echo $row["tech_leader"]; ?></td>
-                        <td class="cell"><?php echo $row["username"]; ?></td>
-                        <td class="cell"><?php echo $row["tech_clockin"]; ?></td>
-                        <td class="cell"><?php echo $row["tech_clockout"]; ?></td>
-                        <td class="cell"><?php echo $row["technician_out"]; ?></td>
-                        <td class="cell"><?php echo $row["technician_in"]; ?></td>
+                        <td data-label="Leader"><?php echo $row["tech_leader"]; ?></td>
+                        <td data-label="Assistant"><?php echo $row["username"]; ?></td>
+                        <td data-label="Clock In"><?php echo $row["tech_clockin"]; ?></td>
+                        <td data-label="Clock Out"><?php echo $row["tech_clockout"]; ?></td>
+                        <td data-label="Rest Out"><?php echo $row["technician_out"]; ?></td>
+                        <td data-label="Rest In"><?php echo $row["technician_in"]; ?></td>
                     </tr>
                 </tbody>
                 
@@ -126,27 +124,7 @@
                 </div>
     </form>
             </div>
-            </div>
-            
-            <script>
-                let arrow = document.querySelectorAll(".arrow");
-                for (var i = 0; i < arrow.length; i++) 
-                    {
-                        arrow[i].addEventListener("click", (e)=>
-                            {
-                                let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
-                                arrowParent.classList.toggle("showMenu");
-                            });
-                    }
-                let sidebar = document.querySelector(".sidebar");
-                let sidebarBtn = document.querySelector(".bx-menu");
-                console.log(sidebarBtn);
-                sidebarBtn.addEventListener("click", ()=>
-                    {
-                        sidebar.classList.toggle("close");
-                    });
-            </script>
-                
+            </div>    
         </div>
     </div>
    
