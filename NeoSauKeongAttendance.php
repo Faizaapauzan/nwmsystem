62f65d815d624fea005f3f366f771a82c18575c3
Faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2022-12-02 11:46:38 +0800

    faiza 2/12/2022

--- NeoSauKeongAttendance.php
+++ NeoSauKeongAttendance.php
@@ -2,10 +2,9 @@
  
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Attendance And Rest Hour</title>
     <link rel = "icon" href = "https://i.ibb.co/ngKJ7c4/android-chrome-512x512.png" type = "image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
