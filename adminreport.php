4929d616dccaf5e6fdcc93d7b6ffec0b6e4b5202
Faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2022-12-14 11:51:23 +0800

    faiza 14/12/2022

--- adminreport.php
+++ adminreport.php
@@ -221,7 +221,12 @@
                                         if(isset($_GET['DateAssign']))
                                             {
                                                 $DateAssign = $_GET['DateAssign'];
-                                                $query = mysqli_query($conn, "SELECT * FROM job_register LEFT JOIN assistants ON job_register.jobregister_id=assistants.jobregister_id WHERE job_register.DateAssign='$DateAssign' AND job_register.job_cancel = '' OR job_register.DateAssign='$DateAssign' AND job_register.job_cancel IS NULL ORDER BY job_assign ASC, departure_timestamp ASC");
+                                                $query = mysqli_query($conn, "SELECT * FROM job_register LEFT JOIN assistants 
+                                                                                ON job_register.jobregister_id=assistants.jobregister_id WHERE 
+                                                                                job_register.DateAssign='$DateAssign' AND job_register.DateAssign !='' AND job_register.job_cancel = '' 
+                                                                                    OR 
+                                                                                job_register.DateAssign='$DateAssign' AND job_register.DateAssign !='' AND job_register.job_cancel IS NULL 
+                                                                                ORDER BY job_assign ASC, departure_timestamp ASC");
                                                 if(mysqli_num_rows($query) > 0)
                                                   {
                                                     foreach($query as $row)
