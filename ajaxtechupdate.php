71849000539173257cfb1178259d397ecea31489
Faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2022-12-26 15:01:31 +0800

    faiza 26/12/2022

--- ajaxtechupdate.php
+++ ajaxtechupdate.php
@@ -1,29 +1,4 @@
-<?php
-    session_start();
-
-    date_default_timezone_set("Asia/Kuala_Lumpur");
-
-    $today_date = date("d-m-Y");
-    $_SESSION['storeDate'] = $today_date;
-
-    $departure_time = date('d-m-Y g:i A');
-    $_SESSION['departuretime'] = $departure_time;
-
-	$arrival_time = date('d-m-Y g:i A');
-    $_SESSION['arrivaltime'] = $arrival_time;
-
-	$leaving_time = date('d-m-Y g:i A');
-    $_SESSION['leavingtime'] = $leaving_time;
-    
-    $rest_out = date('g:i A');
-    $_SESSION['restout'] = $rest_out;
-
-	$rest_in = date('g:i A');
-    $_SESSION['restin'] = $rest_in;
-    
-    $TimeStamp = date('H:i:s');
-    $_SESSION['timestamp'] = $TimeStamp; 
-?>
+<?php session_start(); ?>
 
 <!DOCTYPE html>
 
@@ -60,21 +35,21 @@
         <input type="hidden" name="jobregister_id" value="<?php echo $row['jobregister_id'] ?>">
         
 		<label>Departure time</label>
-        <input type="hidden" name="technician_departure" value="<?php echo $_SESSION["departuretime"] ?>">
+        <input type="hidden" name="technician_departure" value="<?php date_default_timezone_set('Asia/Kuala_Lumpur'); echo $technician_departure = date('d-m-Y g:i A')?>">
 
-		<input type="hidden" name="DateAssign" value="<?php echo $_SESSION["storeDate"] ?>">
-        <input type="hidden" name="departure_timestamp" value="<?php echo $_SESSION["timestamp"] ?>">
+		<input type="hidden" name="DateAssign" value="<?php date_default_timezone_set('Asia/Kuala_Lumpur'); echo $DateAssign = date('d-m-Y') ?>">
+        <input type="hidden" name="departure_timestamp" value="<?php date_default_timezone_set('Asia/Kuala_Lumpur'); echo $departure_timestamp = date('H:i:s') ?>">
         <input type="hidden" name="job_status" value="Doing">
         
 		<div class="input-group mb-3">
             <input readonly type="text" class="form-control" id="Departure" value="<?php echo $row['technician_departure']?>" aria-describedby="basic-addon2">
             <div class="input-group-append">
-                <button id="update_techstatus" name="update_techstatus" class="buttonbiru update" onclick="doSomething(); doSomethingElse(); departureTechnician()" type="button">Departure</button>
+                <button id="update_techstatus" name="update_techstatus" class="buttonbiru update" onclick="DepartTime(); TechnicianDeparture();">Departure</button>
             </div>
             
 			<!-- Generate departure time -->
 			<script type="text/javascript">
-                function doSomething() {
+                function DepartTime() {
                     $.ajax({
                         url: "departureTime.php",
                         success: function(result) {$("#Departure").val(result);}
@@ -82,50 +57,26 @@
                 }
             </script>
             
-			<!-- change job status to doing -->
-            <script type="text/javascript">
-                function doSomethingElse() {
-                    var jobregister_id = $('input[name=jobregister_id]').val();
+			<!-- Update in Job Register Table -->
+			<script type="text/javascript">
+                function TechnicianDeparture() {
+                    var technician_departure = $('input[name=technician_departure]').val();
                     var departure_timestamp = $('input[name=departure_timestamp]').val();
                     var DateAssign = $('input[name=DateAssign]').val();
                     var job_status = $('input[name=job_status]').val();
-                    
-					if (departure_timestamp != '' || departure_timestamp == '', 
-						     jobregister_id != '' || jobregister_id == '', 
-						         DateAssign != '' || DateAssign == '', 
-								 job_status != '' || job_status == '') 
-						
-						{
-							var formData = {departure_timestamp: departure_timestamp,
-                            					 jobregister_id: jobregister_id,
-                            						 DateAssign: DateAssign,
-                            						 job_status: job_status};
-                        
-							$.ajax({
-								url: "changeStatus.php",
-								type: 'POST',
-								data: formData,
-								success: function(response) 
-									{
-										var res = JSON.parse(response);
-										console.log(res);
-									}
-							});
-                    	}
-                }
-            </script>
-            
-			<!-- Update in Job Register Table -->
-			<script type="text/javascript">
-                function departureTechnician() {
-                    var technician_departure = $('input[name=technician_departure]').val();
                     var jobregister_id = $('input[name=jobregister_id]').val();
                     
-					if (technician_departure != '' || technician_departure == '', 
-							  jobregister_id != '' || jobregister_id == '') 
+					if (technician_departure != '' || technician_departure == '',
+                         departure_timestamp != '' || departure_timestamp == '',
+                                  DateAssign != '' || DateAssign == '',
+                                  job_status != '' || job_status == '',
+                              jobregister_id != '' || jobregister_id == '') 
 						
 						{
 							var formData = {technician_departure: technician_departure,
+                                             departure_timestamp: departure_timestamp,
+                                                      DateAssign: DateAssign,
+                                                      job_status: job_status,
                             					  jobregister_id: jobregister_id};
                         
 							$.ajax({
@@ -143,12 +94,12 @@
         </div>
         
 		<label>Time at site</label>
-        <input type="hidden" name="technician_arrival" value="<?php echo $_SESSION["arrivaltime"] ?>">
+        <input type="hidden" name="technician_arrival" value="<?php date_default_timezone_set('Asia/Kuala_Lumpur'); echo $technician_arrival = date('d-m-Y g:i A') ?>">
         
 		<div class="input-group mb-3">
             <input readonly type="text" class="form-control" id="arrival" value="<?php echo $row['technician_arrival']?>" aria-describedby="basic-addon2">
             <div class="input-group-append">
-                <button style="padding-left: 64px;" id="arrivaltechnician" class="buttonbiru" onclick="ArrivalTime();arrivalTechnician();" type="button">Arrival</button>
+                <button style="padding-left: 64px;" id="arrivaltechnician" class="buttonbiru" onclick="ArrivalTime(); TechnicianArrival();" type="button">Arrival</button>
             </div>
             
 			<!-- Generate arrival time -->
@@ -163,7 +114,7 @@
             
 			<!-- Save arrival time in job register table -->
 			<script type="text/javascript">
-                function arrivalTechnician() {
+                function TechnicianArrival() {
                     var technician_arrival = $('input[name=technician_arrival]').val();
                     var jobregister_id = $('input[name=jobregister_id]').val();
                     
@@ -190,17 +141,17 @@
         </div>
         
 		<label>Return time</label>
-        <input type="hidden" name="technician_leaving" value="<?php echo $_SESSION["leavingtime"] ?>">
+        <input type="hidden" name="technician_leaving" value="<?php date_default_timezone_set('Asia/Kuala_Lumpur'); echo $technician_leaving = date('d-m-Y g:i A') ?>">
         
 		<div class="input-group mb-3">
             <input readonly type="text" class="form-control" id="leaving" value="<?php echo $row['technician_leaving']?>" aria-describedby="basic-addon2">
             <div class="input-group-append">
-                <button style="padding-left: 51px;" class="buttonbiru" onclick="test3();leavingTechnician();" type="button">Leaving</button>
+                <button style="padding-left: 51px;" class="buttonbiru" onclick="LeavingTime(); TechnicianLeaving();" type="button">Leaving</button>
             </div>
             
 			<!-- generate leaving time -->
 			<script type="text/javascript">
-                function test3() {
+                function LeavingTime() {
                     $.ajax({
                         url: "departureTime.php",
                         success: function(result) {
@@ -212,7 +163,7 @@
             
 			<!-- save leaving time in job register table -->
 			<script type="text/javascript">
-                function leavingTechnician() {
+                function TechnicianLeaving() {
                     var technician_leaving = $('input[name=technician_leaving]').val();
                     var jobregister_id = $('input[name=jobregister_id]').val();
                     
@@ -240,20 +191,20 @@
         
 		<label>Rest Hour</label>
 		<input type="hidden" name="technician" value="<?php echo $_SESSION['username'] ?>">
-        <input type="hidden" name="today_date" value="<?php echo $_SESSION['storeDate'] ?>">
+        <input type="hidden" name="today_date" value="<?php date_default_timezone_set('Asia/Kuala_Lumpur'); echo $today_date = date('d-m-Y') ?>">
         
-		<input type="hidden" name="tech_out" value="<?php echo $_SESSION["restout"] ?>">
-        <input type="hidden" name="technician_out" value="<?php echo $_SESSION["restout"] ?>">
+		<input type="hidden" name="tech_out" value="<?php date_default_timezone_set('Asia/Kuala_Lumpur'); echo $tech_out = date('g:i A') ?>">
+        <input type="hidden" name="technician_out" value="<?php date_default_timezone_set('Asia/Kuala_Lumpur'); echo $technician_out = date('g:i A') ?>">
        
 		<div class="input-group mb-3">
-			<input readonly type="text" style="position: static;" class="form-control" id="tech_out" value="<?= $row['tech_out']; ?>" aria-describedby="basic-addon2">
+			<input readonly type="text" style="position: static;" class="form-control" id="tech_out" value="<?php echo $row['tech_out']?>" aria-describedby="basic-addon2">
 			<div class="input-group-append">
-                <button class="buttonbiru" onclick="tech_outs();RestOut();RestOutUpdate();" style="position: static; width: fit-content;" type="button">OUT</button>
+                <button class="buttonbiru" onclick="OutTime(); TechnicianOut(); TechnicianOutUpdate();" style="position: static; width: fit-content;" type="button">OUT</button>
             </div>
             
 			<!-- Generate rest out time -->
 			<script type="text/javascript">
-                function tech_outs() {
+                function OutTime() {
                     $.ajax({
                         url: "techresthourtime.php",
                         success: function(result) {
@@ -265,7 +216,7 @@
             
 			<!-- save rest out time in job register table -->
 			<script type="text/javascript">
-                function RestOut() {
+                function TechnicianOut() {
                     var tech_out = $('input[name=tech_out]').val();
                     var jobregister_id = $('input[name=jobregister_id]').val();
                     
@@ -292,7 +243,7 @@
 
             <!-- save rest out time in tech_update table -->
 			<script type="text/javascript">
-                function RestOutUpdate() {
+                function TechnicianOutUpdate() {
                     var technician_out = $('input[name=technician_out]').val();
                     var tech_leader = $('input[name=technician]').val();
                     var techupdate_date = $('input[name=today_date]').val();
@@ -322,17 +273,17 @@
         </div>
         
 		<div class="input-group mb-3">
-            <input type="hidden" name="tech_in" value="<?php echo $_SESSION["restin"] ?>">
-            <input type="hidden" name="technician_in" value="<?php echo $_SESSION["restin"] ?>">
+            <input type="hidden" name="tech_in" value="<?php date_default_timezone_set('Asia/Kuala_Lumpur'); echo $tech_in = date('g:i A') ?>">
+            <input type="hidden" name="technician_in" value="<?php date_default_timezone_set('Asia/Kuala_Lumpur'); echo $technician_in = date('g:i A') ?>">
 
-			<input readonly type="text" style="position: static;" class="form-control" id="tech_in" value="<?= $row['tech_in']; ?>" aria-describedby="basic-addon2">
+			<input readonly type="text" style="position: static;" class="form-control" id="tech_in" value="<?php echo $row['tech_in']?>" aria-describedby="basic-addon2">
 			<div class="input-group-append">
-                <button class="buttonbiru" onclick="tech_ins(); RestIn(); RestInUpdate();" style="position: static; width: fit-content; padding-left: 55px;" type="button">IN</button>
+                <button class="buttonbiru" onclick="InTime(); TechnicianIn(); TechnicianInUpdate();" style="position: static; width: fit-content; padding-left: 55px;" type="button">IN</button>
             </div>
             
 			<!-- Generate rest in time -->
 			<script type="text/javascript">
-                function tech_ins() {
+                function InTime() {
                     $.ajax({
                         url: "techresthourtime.php",
                         success: function(result) {
@@ -344,7 +295,7 @@
 
 			<!-- save rest our in job register table -->
 			<script type="text/javascript">
-                function RestIn() {
+                function TechnicianIn() {
                     var tech_in = $('input[name=tech_in]').val();
                     var jobregister_id = $('input[name=jobregister_id]').val();
                     
@@ -371,7 +322,7 @@
 
 			<!-- save rest our in tech_update table -->
 			<script type="text/javascript">
-                function RestInUpdate() {
+                function TechnicianInUpdate() {
                     var technician_in = $('input[name=technician_in]').val();
                     var tech_leader = $('input[name=technician]').val();
                     var techupdate_date = $('input[name=today_date]').val();
--- departureupdate.php
+++ departureupdate.php
@@ -6,10 +6,20 @@
     
     if(isset($_POST['jobregister_id']) && $_POST['jobregister_id']!='' || $_POST['jobregister_id']==''
         &&
-       isset($_POST['technician_departure']) && $_POST['technician_departure']!='' || $_POST['technician_departure']=='')
+       isset($_POST['technician_departure']) && $_POST['technician_departure']!='' || $_POST['technician_departure']==''
+        &&
+       isset($_POST['departure_timestamp']) && $_POST['departure_timestamp']!='' || $_POST['departure_timestamp']==''
+        &&
+       isset($_POST['DateAssign']) && $_POST['DateAssign']!='' || $_POST['DateAssign']==''
+        &&
+       isset($_POST['job_status']) && $_POST['job_status']!='' || $_POST['job_status']=='')
         
         {
-            $sql = "UPDATE job_register SET technician_departure ='".addslashes($_POST['technician_departure'],)."'
+            $sql = "UPDATE job_register SET 
+                           technician_departure ='".addslashes($_POST['technician_departure'],)."',
+                           departure_timestamp ='".addslashes($_POST['departure_timestamp'],)."',
+                           DateAssign ='".addslashes($_POST['DateAssign'],)."',
+                           job_status ='".addslashes($_POST['job_status'],)."'
                     WHERE jobregister_id='".addslashes($_POST['jobregister_id'])."'";
             
             if($conn->query($sql))
