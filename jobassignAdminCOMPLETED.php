69c9c444264eaeeecb617b1c74e3a032dac4d840
faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2023-08-01 14:41:50 +0800

    jia sheng 1/8/2023

--- jobassignAdminCOMPLETED.php
+++ jobassignAdminCOMPLETED.php
@@ -15,6 +15,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
+
+
 </head>
 
 <body>
