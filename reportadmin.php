9b21fcfed01aec163bdbe164b22c849edabb1970
faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2023-08-24 08:38:51 +0800

    jia sheng 24/8/2023

--- reportadmin.php
+++ reportadmin.php
@@ -115,8 +115,6 @@
             <div class="CodeDropdown" style="margin-right: 20px;margin-left: 21px;">
                 <label for="date" class="details" style="padding-right: 20px;">Date</label>
                 <input id="myInput" placeholder="DD - MM - YYYY" type="text" style="height: 24px; width: auto;" name="DateAssign" value="<?php if(isset($_GET['DateAssign'])){echo $_GET['DateAssign'];} else { echo $date = date('d-m-Y'); } ?>" class="form-control">
-                <button type="submit" id="submit" class="btn-biru" style="width: auto;">Submit</button>
-                <button class="btn-biru" id="clear" style="width: auto;" onclick="document.getElementById('myInput').value = ''">Clear</button>
             </div>
 
             <!-- Job Update Time -->
