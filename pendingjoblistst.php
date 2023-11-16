8a3ead6fa1946efbc54c8537a3e4b34a45d563e5
Faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2023-11-16 09:55:54 +0800

    Faiza 16/11/2023

--- pendingjoblistst.php
+++ pendingjoblistst.php
@@ -78,8 +78,8 @@
 							
 							include 'dbconnect.php';
 							
-							$numRow = "SELECT * FROM job_register WHERE (job_status = 'Pending' AND job_cancel = '' AND staff_position != 'Storekeeper') 
-                                       OR (job_status = 'Pending' AND job_cancel IS NULL AND staff_position != 'Storekeeper')";
+							$numRow = "SELECT * FROM job_register WHERE (job_status = 'Pending' AND job_cancel = '') 
+                                       OR (job_status = 'Pending' AND job_cancel IS NULL)";
 
                             $numRow_run = mysqli_query($conn, $numRow);
                                     
@@ -97,8 +97,8 @@
 						
 						include 'dbconnect.php';
 						
-						$results = $conn->query("SELECT * FROM job_register WHERE (job_status = 'Pending' AND job_cancel = '' AND staff_position != 'Storekeeper') 
-                                                 OR (job_status = 'Pending' AND job_cancel IS NULL AND staff_position != 'Storekeeper')
+						$results = $conn->query("SELECT * FROM job_register WHERE (job_status = 'Pending' AND job_cancel = '') 
+                                                 OR (job_status = 'Pending' AND job_cancel IS NULL)
                                                  ORDER BY jobregisterlastmodify_at DESC");
                                         
                     	while ($row = $results->fetch_assoc()) {
