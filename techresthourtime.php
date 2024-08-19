b9e4e3c0d05630522c3613c411362f9f7b8848af
Faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2024-08-19 09:04:51 +0800

    Faiza 19/8/2024

--- techresthourtime.php
+++ techresthourtime.php
@@ -1,4 +1,23 @@
-<?php                        
-    date_default_timezone_set("Asia/Kuala_Lumpur");
-    echo date('g:i A'); //Returns IST
+<?php
+    
+    include "dbconnect.php";
+    
+    $sql = "SELECT CURRENT_TIME() AS server_time";
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $server_time = $row['server_time'];
+        
+        $formatted_time = date('g:i A', strtotime($server_time));
+        
+        echo $formatted_time;
+    }
+    
+    else {
+        echo "Error fetching server time.";
+    }
+    
+    $conn->close();
+    
 ?>
